71eea728765855c0b67aa6486fe18d5c3e6d1dd0
Rahul Lohra <rahullohra987@gmail.com>
2022-10-14 20:41:45 +0530

    Navs Active

--- StartListening/StartListening-main.js
+++ StartListening/StartListening-main.js
@@ -1,14 +1,49 @@
 'use strict'
 
-const active = document.querySelector('.active')
-const navsBtn = document.querySelector('.navbtn')
+let active = document.querySelector('.active')
+const navsBtn = document.querySelector('.navs')
 const cards = document.querySelector('.song-cards')
 const threeDot = document.querySelector('.three-dot')
 const playBtn = document.getElementsByClassName('.play')
+const homeBtn = document.querySelector('.homebtn')
+const playListBtn = document.querySelector('.playListbtn')
+const favBtn = document.querySelector('.favbtn')
+
 
 
 cards.addEventListener('mouseover', (event) => {
     event.preventDefault();
     threeDot.style.display = 1;
     playBtn.style.display = 1
+})
+
+
+homeBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    active = document.querySelector('.active')
+    if (!event.target.classList.contains('active')) {
+        active.classList.remove('active')
+        event.target.classList.add('active')
+    }
+
+})
+
+favBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    active = document.querySelector('.active')
+    if (!event.target.classList.contains('active')) {
+        active.classList.remove('active')
+        event.target.classList.add('active')
+    }
+
+})
+
+playListBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    active = document.querySelector('.active')
+    if (!event.target.classList.contains('active')) {
+        active.classList.remove('active')
+        event.target.classList.add('active')
+    }
+
 })
