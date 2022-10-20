ea769f75d0258714a5a5417cebd60e73d1c78518
Rahul Lohra <rahullohra987@gmail.com>
2022-10-20 22:16:01 +0530

    trying to make background image auto changeble

--- StartListening/StartListening-main.js
+++ StartListening/StartListening-main.js
@@ -60,12 +60,12 @@ function scrollFunction() {
     }
 }
 
-// playBtn.addEventListener('click', (event) => {
-//     event.preventDefault()
-//     document.querySelector('.mainbar-backgroung-page2-img').style.display = '1'
-//     document.querySelector('.mainbar-backgroung-page1-img').style.display = 'none'
+playBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    document.querySelector('.mainbar-backgroung-page2-img').style.display = '1'
+    document.querySelector('.mainbar-backgroung-page1-img').style.display = 'none'
 
-// })
+})
 
 
 // Changing background Picture
@@ -74,30 +74,52 @@ let artistList = ['arijit', 'badshah', 'shreya']
 
 let i = 0
 
-let counter = () => {
-    let len = artistList.length
-    while (i < len) {
-        if (i = len - 1) {
-            i = 0
-        } else {
-            i += 1
-            return i
-        }
-    }
-}
+// let counter = () => {
+//     let len = artistList.length
+//     while (i < len) {
+//         if (i = len - 1) {
+//             i = 0
+//         } else {
+//             i += 1
+//             return i
+//         }
+//     }
+// }
 
 const changingBackground = (index) => {
     document.querySelector('.mainbar-backgroung-page1-img').style.backgroundImage = `url('../img/${artistList[index]}.jpg')`
 }
 
 
-const loop = () => {
-    setTimeout(function() {
-        let count = counter()
-        console.log(i, count, artistList[count]);
-        changingBackground(count)
-        loop()
-    }, 4000); //9000 = 9000ms = 9s
-};
+// const loop = () => {
+//     setTimeout(function() {
+//         let count = counter()
+//         console.log(i, count, artistList[count]);
+//         changingBackground(count)
+//         loop()
+//     }, 4000); //9000 = 9000ms = 9s
+// };
 
-loop()
+// loop()
+
+
+const looper = function() {
+    setTimeout(() => {
+        if (i < artistList.length) {
+            changingBackground(i)
+            i += 1
+        }
+        display()
+    }, 2000)
+}
+
+
+const display = () => {
+    if (i === artistList.length) {
+        i = 0
+    }
+    looper()
+
+}
+
+// display()
