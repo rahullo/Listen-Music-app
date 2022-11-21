844dfbd6cd24cba377c5fea001402c1b5097dd8b
Rahul Lohra <rahullohra987@gmail.com>
2022-11-21 17:55:50 +0530

    Implimenting background changing

--- StartListening/StartListening-main.js
+++ StartListening/StartListening-main.js
@@ -1,22 +1,19 @@
 'use strict'
 
-let active = document.querySelector('.active')
+// Navs buttons
 const navsBtn = document.querySelector('.navs')
-const cards = document.querySelector('.song-cards')
-const threeDot = document.querySelector('.three-dot')
-const playBtn = document.querySelector('.play')
 const homeBtn = document.querySelector('.homebtn')
 const playListBtn = document.querySelector('.playListbtn')
 const favBtn = document.querySelector('.favbtn')
+let active = document.querySelector('.active')
+
+//Cards button
+const cards = document.querySelector('.song-cards')
+const threeDot = document.querySelector('.three-dot')
+const playBtn = document.querySelector('.play')
 
 
-
-cards.addEventListener('mouseover', (event) => {
-    event.preventDefault();
-    threeDot.style.display = 1;
-    playBtn.style.display = 1
-})
-
+//Navs feature implementation
 
 homeBtn.addEventListener('click', (event) => {
     event.preventDefault()
@@ -25,8 +22,19 @@ homeBtn.addEventListener('click', (event) => {
         active.classList.remove('active')
         event.target.classList.add('active')
     }
-    document.querySelector('.mainbar-backgroung-page2-img').style.display = 'none'
-    document.querySelector('.mainbar-backgroung-page1-img').style.display = '1'
+    document.querySelector('.mainbar-background-page1-img').style.display = '1'
+    document.querySelector('.mainbar-background-page2-img').style.display = 'none'
+
+})
+
+
+playListBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    active = document.querySelector('.active')
+    if (!event.target.classList.contains('active')) {
+        active.classList.remove('active')
+        event.target.classList.add('active')
+    }
 
 })
 
@@ -40,67 +48,46 @@ favBtn.addEventListener('click', (event) => {
 
 })
 
-playListBtn.addEventListener('click', (event) => {
-    event.preventDefault()
-    active = document.querySelector('.active')
-    if (!event.target.classList.contains('active')) {
-        active.classList.remove('active')
-        event.target.classList.add('active')
-    }
 
+//Cards feature implementation
+
+
+cards.addEventListener('mouseover', (event) => {
+    event.preventDefault();
+    console.log("Mouse hover happen!!!");
+    threeDot.style.display = 1;
+    playBtn.style.display = 1
 })
 
-window.onscroll = function() { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-50px";
-    }
-}
+// window.onscroll = function() { scrollFunction() };
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("navbar").style.top = "0";
+//     } else {
+//         document.getElementById("navbar").style.top = "-50px";
+//     }
+// }
 
 playBtn.addEventListener('click', (event) => {
     event.preventDefault()
-    document.querySelector('.mainbar-backgroung-page2-img').style.display = '1'
-    document.querySelector('.mainbar-backgroung-page1-img').style.display = 'none'
+    console.log("Playbtn clicked!!");
+    document.querySelector('.mainbar-background-page1-img').style.display = 'none'
+    document.querySelector('.mainbar-background-page2-img').style.display = '1'
 
 })
 
 
 // Changing background Picture
 
-let artistList = ['arijit', 'badshah', 'shreya']
+let artistList = ['arijit', 'badshah', 'shreya', 'honeysingh', 'jubin', 'justinbieber', 'neha', 'palak', 'salim']
 
 let i = 0
 
-// let counter = () => {
-//     let len = artistList.length
-//     while (i < len) {
-//         if (i = len - 1) {
-//             i = 0
-//         } else {
-//             i += 1
-//             return i
-//         }
-//     }
-// }
-
 const changingBackground = (index) => {
-    document.querySelector('.mainbar-backgroung-page1-img').style.backgroundImage = `url('../img/${artistList[index]}.jpg')`
+    document.querySelector('.mainbar-background-page1-img').style.backgroundImage = `url('../img/artistimg/${artistList[index]}.jpg')`
 }
-
-
-// const loop = () => {
-//     setTimeout(function() {
-//         let count = counter()
-//         console.log(i, count, artistList[count]);
-//         changingBackground(count)
-//         loop()
-//     }, 4000); //9000 = 9000ms = 9s
-// };
-
-// loop()
 
 
 const looper = function() {
